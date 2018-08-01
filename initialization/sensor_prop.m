f1b78e1d75202d1c7e42c3dd7bd86d9353edaac6
nhz2 <nhz2@cornell.edu>
2018-08-01 16:13:26 -0400

    More Gyro data

--- initialization/sensor_prop.m
+++ initialization/sensor_prop.m
@@ -1,6 +1,9 @@
 function init=sensor_prop(init)
 %adds sensor parameters to init
 
+init.GPS_ODR=104*8/120;
+%GPS output data rate in Hz
+
 init.IMU0_DCM_BODY= eye(3);
 %DCM to translate from body basis to IMU0 basis
 init.IMU0_POS_BODY= zeros([3 1]);
@@ -9,19 +12,19 @@ init.IMU0_MAG_FS= 4.0;
 %The fullscale of the magnetometer is +- this (Gauss)
 init.IMU0_MAG_GN= 6842.0;
 %what is read when 1 gauss is applied
-init.IMU0_MAG_ODR= 10.0;
+init.IMU0_MAG_ODR= 832.0/12.0;
 %Mag output data rate in Hz also used for the temp sensor
 init.IMU0_MAG_ZGAUSS= zeros([3 1]);
 %gauss applied to get 0 output
-init.IMU0_MAG_RMS= transpose([3.2e-3,3.2e-3,4.1e-3]);
+init.IMU0_MAG_RMS= zeros([3 1]);%transpose([3.2e-3,3.2e-3,4.1e-3]);
 %RMS noise in gauss
-init.IMU0_G_ODR= 100.0;
+init.IMU0_G_ODR= 1664;
 %Gyro output data rate in Hz
-init.IMU0_G_TYOFF= zeros([3 1]);%transpose([0.17,0.17,0.17]);
+init.IMU0_G_TYOFF= transpose([0.05,0.05,0.05]);
 %gyro offset, what the gyro reads in rad/s when not rotating
 init.IMU0_G_GN= 13096.0;
 %what is read when 1 rad/s
 init.IMU0_G_FS= 2.182;
 %The fullscale of the gyro is +- this (rad/s)
-init.IMU0_G_RMS= transpose([1.2e-3,1.2e-3,1.2e-3]);
+init.IMU0_G_RMS= transpose([5e-3,5e-3,5e-3]);
 %RMS noise in rad/s
