b9eff928e06f33e4207f38a69bc1c9d07f4e3e18
nhz2 <nhz2@cornell.edu>
2018-10-14 14:07:38 -0400

    Changed sensor odrs

--- initialization/sensor_prop.m
+++ initialization/sensor_prop.m
@@ -1,7 +1,7 @@
 function init=sensor_prop(init)
 %adds sensor parameters to init
 
-init.GPS_ODR=104*8/120;
+init.GPS_ODR=10;
 %GPS output data rate in Hz
 
 init.IMU0_DCM_BODY= eye(3);
@@ -12,13 +12,13 @@ init.IMU0_MAG_FS= 4.0;
 %The fullscale of the magnetometer is +- this (Gauss)
 init.IMU0_MAG_GN= 6842.0;
 %what is read when 1 gauss is applied
-init.IMU0_MAG_ODR= 832.0/12.0;
+init.IMU0_MAG_ODR= 10;
 %Mag output data rate in Hz also used for the temp sensor
 init.IMU0_MAG_ZGAUSS= zeros([3 1]);
 %gauss applied to get 0 output
 init.IMU0_MAG_RMS= zeros([3 1]);%transpose([3.2e-3,3.2e-3,4.1e-3]);
 %RMS noise in gauss
-init.IMU0_G_ODR= 1664;
+init.IMU0_G_ODR= 100;
 %Gyro output data rate in Hz
 init.IMU0_G_TYOFF= transpose([0.05,0.05,0.05]);
 %gyro offset, what the gyro reads in rad/s when not rotating
